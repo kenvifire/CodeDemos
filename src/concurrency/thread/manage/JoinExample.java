c325c26a8f607d123fa493be636e25ce39113e8a
kenvifire <mrwhite@163.com>
2013-01-09 23:21:05 +0800

    test file

---
+++ src/concurrency/thread/manage/JoinExample.java
@@ -0,0 +1,13 @@
+package concurrency.thread.manage;
+
+public class JoinExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
